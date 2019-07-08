08ed830138614b778f8ba682f1dffbcb087fe169
sanyangsanyang <947106974@qq.com>
2019-07-08 11:14:54 +0800

    add

--- reshape.py
+++ reshape.py
@@ -4,8 +4,8 @@ from PIL import Image
 def make_square(im, min_size=256, fill_color=(0, 0, 0, 0)):
     x, y = im.size
     size = max(min_size, x, y)
-    new_im = Image.new('RGBA', (size, size), fill_color)
-    new_im.paste(im, ((size-x) // 2, (size - y) // 2))
+    new_im = Image.new("RGBA", (size, size), fill_color)
+    new_im.paste(im, ((size - x) // 2, (size - y) // 2))
     return new_im
 
 
