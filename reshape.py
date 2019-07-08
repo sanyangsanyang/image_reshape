991efa053eff9a7204e9e99d7eb5aa50f496972f
sanyangsanyang <947106974@qq.com>
2019-07-08 10:43:27 +0800

    add

--- reshape.py
+++ reshape.py
@@ -1,11 +1,15 @@
-from Pillow import Image
+from PIL import Image
+
+
 def make_square(im, min_size=256, fill_color=(0, 0, 0, 0)):
     x, y = im.size
     size = max(min_size, x, y)
     new_im = Image.new('RGBA', (size, size), fill_color)
-    new_im.paste(im, ((size-x) / 2, (size - y) / 2))
+    new_im.paste(im, ((size-x) // 2, (size - y) // 2))
     return new_im
 
-test_image = Image.open("test.jpg")
+
+test_image = Image.open("test.png")
 new_image = make_square(test_image)
 new_image.show()
+new_image.save("convert.png", new_image.format)
